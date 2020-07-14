d4b7b1c1da573db6b2cfb525cbb6dc80d6cc1f10
Joshua Brook <joshuabrookuk@gmail.com>
2020-07-14 13:47:22 +0100

    RED: Testing for withdrawal method.

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -10,11 +10,19 @@ describe Transaction do
     expect(transaction.deposit(1000)).to eq 1000
   end
 
-  it 'should add the and return the sum of deposits' do
+  it 'should add and return the sum of deposits' do
     transaction = Transaction.new
     transaction.deposit(1000)
     transaction.deposit(2000)
     expect(transaction.balance).to eq 3000
+  end
+
+  it 'should subtract and return the sum of deposits' do
+    transaction = Transaction.new
+    transaction.deposit(1000)
+    transaction.deposit(2000)
+    transaction.withdrawal(500)
+    expect(transaction.balance).to eq 2500
   end
 
 end
