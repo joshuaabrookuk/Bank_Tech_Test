018d85b2d80f5745e2b3da0636621a9f3a328675
Joshua Brook <joshuabrookuk@gmail.com>
2020-07-14 11:10:43 +0100

    REFACTORED: Tried to make a let/ before each block for rspec.

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,7 +1,11 @@
 require 'transaction'
 
+
+
 describe Transaction do
+
   it 'should respond to deposit with integer argument' do
-    expect(subject.deposit(1000)).to eq 1000
+    transaction = Transaction.new
+    expect(transaction.deposit(1000)).to eq 1000
   end
 end
