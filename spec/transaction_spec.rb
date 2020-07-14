5612b2fb7ccff2bfc17349efeca029c3f0d5d560
Joshua Brook <joshuabrookuk@gmail.com>
2020-07-14 11:25:27 +0100

    RED:Transaction should add the and return the sum of deposits.

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,11 +1,18 @@
 require 'transaction'
 
-
-
 describe Transaction do
 
   it 'should respond to deposit with integer argument' do
     transaction = Transaction.new
     expect(transaction.deposit(1000)).to eq 1000
   end
+
+
+  it 'should add the and return the sum of deposits' do
+    transaction = Transaction.new
+    transaction.deposit(1000)
+    transaction.deposit(2000)
+    expect(transaction.balance).to eq 3000
+  end
+
 end
